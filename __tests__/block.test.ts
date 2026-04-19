059ab3a42c863d8da4b1375d1b475728a2fab8a1
Gabriel Valdivia <valdivia.gabrielvictor@proximasolucoes.com.br>
2026-04-19 06:01:43 -0300

    test: adicionar teste para verificar mudança de hash ao alterar nonce

--- __tests__/block.test.ts
+++ __tests__/block.test.ts
@@ -14,8 +14,17 @@ describe("Blockclear", () => {
 
 
   test("deve lançar erro se index for negativo",()=> {
-    expect(() => Block.generate(-1, "dados inválidos", "")).toThrow(
+    expect(() => Block.generate(-1, "dados inválidos", "",0)).toThrow(
       "Index can't be negative"
     );
   });
+
+  test("hash deve mudar quando nonce mudar", ()=> {
+    const block1 = Block.generate(1, "teste", "abc123", 0);
+    const block2 = Block.generate(1, "teste", "abc123", 5);
+
+    expect(block1.hash).not.toBe(block2.hash);
+  })
+
+  
 });
