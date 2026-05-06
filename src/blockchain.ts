5d9b7963631edcd59508c4ec0d71711fea89a977
Gabriel Valdivia <yp4iafp1d@mozmail.com>
2026-05-05 23:32:49 -0300

    feat(blockchain): implementar classe Blockchain com métodos de validação e adição de blocos

---
+++ src/blockchain.ts
@@ -0,0 +1,38 @@
+import Block from "./lib/block";
+
+export default class Blockchain {
+    blocks: Block[];
+    nextIndex: number = 0;
+
+    constructor() {
+        this.blocks = [Block.generateGenesis()]
+        this.nextIndex++;
+    }
+
+    getLastBlock(): Block {
+        return this.blocks[this.blocks.length - 1];
+    }
+
+    addBlock(block: Block, difficulty: number) {
+        const lastBlock = this.getLastBlock();
+
+        if (!block.isValid(lastBlock.hash, lastBlock.index, difficulty)) return false;
+
+        this.blocks.push(block);
+        this.nextIndex++;
+
+        return true;
+    }
+
+    isValid(): boolean {
+        for (let i = 1; i < this.blocks.length; i++) {
+            const currentBlock = this.blocks[i];
+            const previousBlock = this.blocks[i - 1];
+            const result: boolean = currentBlock.isValid(previousBlock.hash, previousBlock.index, 0)
+            if (!result) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
